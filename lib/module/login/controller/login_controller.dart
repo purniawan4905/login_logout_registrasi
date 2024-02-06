be32d502de676437235dcdbdbfc8a809db1fbd59
purniawan4905 <purniawan4905@gmail.com>
2024-02-06 17:45:14 +0700

    update login_controller

--- lib/module/login/controller/login_controller.dart
+++ lib/module/login/controller/login_controller.dart
@@ -1,27 +1,30 @@
-
 import 'package:flutter/material.dart';
 import 'package:login_logout_regitrasi/core.dart';
+import 'package:login_logout_regitrasi/service/auth_service/auth_service.dart';
 import '../view/login_view.dart';
 
 class LoginController extends State<LoginView> {
-    static late LoginController instance;
-    late LoginView view;
+  static late LoginController instance;
+  late LoginView view;
 
-    @override
-    void initState() {
-        instance = this;
-        super.initState();
-    }
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
 
-    @override
-    void dispose() => super.dispose();
+  @override
+  void dispose() => super.dispose();
 
-    @override
-    Widget build(BuildContext context) => widget.build(context, this);
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
 
-    String? email;
-    String? password;
-    login() {}
+  String? email;
+  String? password;
+  login() async {
+    bool isLoggedIn = await AuthService().loginByEmail(
+      email: email!,
+      password: password!,
+    );
+  }
 }
-        
-    
