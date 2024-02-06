cb1a5ca905b54c31067aafe9639a0d38ad15983b
purniawan4905 <purniawan4905@gmail.com>
2024-02-06 14:42:51 +0700

    membuat login_view.dart

--- lib/module/login/view/login_view.dart
+++ lib/module/login/view/login_view.dart
@@ -17,7 +17,49 @@ class LoginView extends StatefulWidget {
         child: Container(
             padding: const EdgeInsets.all(10.0),
             child: Column(
-            children: const [],
+            children: [
+              TextFormField(
+              initialValue: 'admin@gmail.com',
+              maxLength: 20,
+              decoration: const InputDecoration(
+              labelText: 'Email',
+              labelStyle: TextStyle(
+              color: Colors.blueGrey,
+              ),
+              suffixIcon: Icon(
+              Icons.email,
+              ),
+              enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+              color: Colors.blueGrey,
+              ),
+              ),
+              helperText: 'Enter your email address',
+              ),
+              onChanged: (value) {},
+              ),
+              TextFormField(
+              initialValue: '123456',
+              maxLength: 20,
+              obscureText: true,
+              decoration: const InputDecoration(
+              labelText: 'Password',
+              labelStyle: TextStyle(
+              color: Colors.blueGrey,
+              ),
+              suffixIcon: Icon(
+              Icons.password,
+              ),
+              enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+              color: Colors.blueGrey,
+              ),
+              ),
+              helperText: 'Enter your password',
+              ),
+              onChanged: (value) {},
+              ),
+            ],
             ),
         ),
       ),
