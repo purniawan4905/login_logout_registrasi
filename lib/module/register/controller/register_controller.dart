aaacfa58e4f16f82b9c8f9a3080727dfab7eaf0f
purniawan4905 <purniawan4905@gmail.com>
2024-02-07 10:56:23 +0700

    menambah string? emaol dan password di register_controller

--- lib/module/register/controller/register_controller.dart
+++ lib/module/register/controller/register_controller.dart
@@ -1,23 +1,24 @@
-
 import 'package:flutter/material.dart';
 import 'package:login_logout_regitrasi/core.dart';
 import '../view/register_view.dart';
 
 class RegisterController extends State<RegisterView> {
-    static late RegisterController instance;
-    late RegisterView view;
+  static late RegisterController instance;
+  late RegisterView view;
 
-    @override
-    void initState() {
-        instance = this;
-        super.initState();
-    }
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
 
-    @override
-    void dispose() => super.dispose();
+  @override
+  void dispose() => super.dispose();
 
-    @override
-    Widget build(BuildContext context) => widget.build(context, this);
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+
+  String? email;
+  String? password;
+  register() async {}
 }
-        
-    
--- lib/module/register/view/register_view.dart
+++ lib/module/register/view/register_view.dart
@@ -9,16 +9,84 @@ class RegisterView extends StatefulWidget {
   Widget build(context, RegisterController controller) {
   controller.view = this;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-        actions: const [],
-        ),
-        body: SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
-            ),
+      backgroundColor: Colors.blueGrey[600],
+      body: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      // initialValue: 'admin@gmail.com',
+                      maxLength: 20,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.blueGrey,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.email,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        helperText: 'Enter your email address',
+                      ),
+                      onChanged: (value) {
+                        controller.email = value;
+                      },
+                    ),
+                    TextFormField(
+                      // initialValue: '123456',
+                      maxLength: 20,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.blueGrey,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.password,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        helperText: 'Enter your password',
+                      ),
+                      onChanged: (value) {
+                        controller.password = value;
+                      },
+                    ),
+                    Divider(),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 46,
+                      child: ElevatedButton(
+                        onPressed: () => controller.login(),
+                        child: const Text("Login"),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 46,
+                      child: ElevatedButton(
+                        onPressed: () => Get.to(const RegisterView()),
+                        child: const Text("Register"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
