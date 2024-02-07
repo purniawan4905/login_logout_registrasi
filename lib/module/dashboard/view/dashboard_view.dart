f9df3188343b5443231d785ac83293abbe8ba842
purniawan4905 <purniawan4905@gmail.com>
2024-02-07 11:55:21 +0700

    menmbah banner image

--- lib/module/dashboard/view/dashboard_view.dart
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:login_logout_regitrasi/core.dart';
 import '../controller/dashboard_controller.dart';
 
@@ -7,18 +7,79 @@ class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
 
   Widget build(context, DashboardController controller) {
-  controller.view = this;
+    controller.view = this;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
-            ),
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                height: 160.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      16.0,
+                    ),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 20.0,
+                      top: 0.0,
+                      bottom: 0.0,
+                      child: SizedBox(
+                        width: 100.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "30%",
+                              style: GoogleFonts.oswald(
+                                fontSize: 30.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Discount Only Valid for Today",
+                              style: GoogleFonts.oswald(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -27,4 +88,3 @@ class DashboardView extends StatefulWidget {
   @override
   State<DashboardView> createState() => DashboardController();
 }
-    
