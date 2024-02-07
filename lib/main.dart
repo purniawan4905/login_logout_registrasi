e4b7e044abd508d15781288c7955781634351acc
purniawan4905 <purniawan4905@gmail.com>
2024-02-07 12:23:47 +0700

    delete comment script, login logout finish

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ import 'package:login_logout_regitrasi/module/login/view/login_view.dart';
 import 'package:login_logout_regitrasi/state_util.dart';
 
 void main() async {
-WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,23 +20,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-navigatorKey: Get.navigatorKey,
+      navigatorKey: Get.navigatorKey,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -45,6 +30,3 @@ navigatorKey: Get.navigatorKey,
     );
   }
 }
-
-
-
